17ab664a194fe0b650eb6551dd23f1a2e06a395e
João Antunes <joaofbantunes@gmail.com>
2018-09-15 12:37:14 +0100

    Adding project to source control

---
+++ src/CodingMilitia.ExcelDataReaderSample.App/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using ExcelDataReader;
+
+namespace CodingMilitia.ExcelDataReaderSample.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // required because of known bug when running on .NET Core
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var stream = File.Open("Files/sample.xlsx", FileMode.Open, FileAccess.Read))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                DataTableCollection sheets = reader.AsDataSet(GetDataSetConfig()).Tables;
+                DataTable sheet = sheets["Sheet1"];
+                foreach (DataRow row in sheet.Rows)
+                {
+                    Console.WriteLine("{0}: {1}", row["Id"], row["Description"]);
+                }
+
+            }
+        }
+
+        private static ExcelDataSetConfiguration GetDataSetConfig()
+        {
+            return new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = _ => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true,
+                    ReadHeaderRow = rowReader => Console.WriteLine("{0}: {1}", rowReader[0], rowReader[1])
+                }
+            };
+        }
+    }
+}
